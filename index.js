46cb2ae2868201ff0343bffcbbbe2db66389ddc5
Ana Ferreira <91092098+anaferreira150965@users.noreply.github.com>
2023-10-18 17:38:18 -0300

    Update index.js
    Alterado para entrega a Felipe

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Felipão");
+nome = str(input("Digite o nome do Herói: "))
+xp = int(input("Digite o XP do Heroi: "))
+
+if xp <= 1000:
+   nivel = "Ferro"
+elif xp <= 2000:
+   nivel = "Bronze"
+elif xp <= 5000:
+   nivel = "Prata "
+elif xp <= 7000: 
+   nivel = "Ouro"
+elif xp <= 8000: 
+   nivel = "Platina"
+elif xp <= 9000:
+   xp = "Ascendente"
+elif xp <= 10000:
+   nivel = "Imortal"
+else:   
+   nivel = "Radiante"
+print(f"O Herói de nome {nome} está no nível {nivel}")
